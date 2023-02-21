52331d2dc3da0c08e138b9a154637390c9af7eb6
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2023-02-21 15:14:11 +0530

    added rightSection

---
+++ src/components/RightSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import rightSection from "../assets/rightSection.jpeg";
+import Features from "./Features";
+import BookRide from "./BookRide";
+import Faq from "./Faq";
+import Statistics from "./Statistics";
+import KnowMore from "./KnowMore";
+import Offer from "./Offer";
+import Video from "./Video";
+
+export default function RightSection() {
+  return (
+    <>
+      <img
+        src={rightSection}
+        alt="Right Section"
+        style={{
+          width: "50vw",
+          height: "85vh",
+        }}
+      />
+      <Offer />
+      <Video />
+      <Features />
+      <BookRide />
+      <Faq />
+      <Statistics />
+      <KnowMore />
+    </>
+  );
+}
