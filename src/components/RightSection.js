7aa3c33ff956562ff01ba1fe1f7a97e69ff1f38d
RudrakshiKumar <118998160+RudrakshiKumar@users.noreply.github.com>
2023-02-22 17:27:38 +0530

    Revert changes from gaurav

--- src/components/RightSection.js
+++ src/components/RightSection.js
@@ -2,28 +2,37 @@ import React from "react";
 import rightSection from "../assets/rightSection.jpeg";
 import Features from "./Features";
 import BookRide from "./BookRide";
-import FAQ from "./FAQ";
+import Faq from "./Faq";
 import Statistics from "./Statistics";
 import KnowMore from "./KnowMore";
 import Offer from "./Offer";
 import Video from "./Video";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import MobileHeader from "./MobileHeader";
 
 export default function RightSection() {
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
-      <img
-        src={rightSection}
-        alt="Right Section"
-        style={{
-          width: "50vw",
-          height: "85vh",
-        }}
-      />
+      {isMatch ? (
+        <MobileHeader />
+      ) : (
+        <img
+          src={rightSection}
+          alt="Right Section"
+          style={{
+            width: "50vw",
+            height: "85vh",
+          }}
+        />
+      )}
       <Offer />
       <Video />
       <Features />
       <BookRide />
-      <FAQ />
+      <Faq />
       <Statistics />
       <KnowMore />
     </>
