5c3da5747281cf1fb93ff8ac948d4e9a476be141
RudrakshiKumar <118998160+RudrakshiKumar@users.noreply.github.com>
2023-02-28 14:51:17 +0530

    Added RideCompletion Component

--- src/components/RideCompletion.js
+++ src/components/RideCompletion.js
@@ -1,7 +1,38 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import { Box } from "@mui/system";
+import { Typography, TextField, Button } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 const RideCompletion = () => {
-  return <div>RideCompletion</div>;
+  return (
+    <div>
+      <Box className="m-auto">
+        {/* <Box
+          component="img"
+          sx={{
+            height: 70,
+            pt: 2,
+            px: 5,
+          }}
+          alt="Your logo."
+          src={logo}
+        /> */}
+        <br />
+        <Box>
+          <CheckCircleIcon sx={{ fontSize: 200, color: "#59CE8F" }} />
+          <Typography sx={{ color: "#59CE8F" }}>
+            Ride Completed Successfully!
+          </Typography>
+          <Button> Download Details</Button> <br />
+          <Button> Rate your experience</Button> <br />
+          <StarBorderIcon /> <StarBorderIcon /> <StarBorderIcon />{" "}
+          <StarBorderIcon /> <StarBorderIcon />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default RideCompletion;
