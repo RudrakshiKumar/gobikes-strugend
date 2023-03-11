740b84bbb4ac85fde8cd4009bbafd98178d379ab
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2023-03-11 14:19:04 +0530

    added functionality

--- src/components/FooterLong.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FooterLong = () => {
-  return <div>FooterLong</div>;
-};
-
-export default FooterLong;
--- src/components/FooterShort.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FooterShort = () => {
-  return <div>FooterShort</div>;
-};
-
-export default FooterShort;
