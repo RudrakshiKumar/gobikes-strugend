a086909d61910c503dd245fd6e29901fe62efbd6
RudrakshiKumar <118998160+RudrakshiKumar@users.noreply.github.com>
2023-03-12 19:17:06 +0530

    Added FAQs functionalities

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -4,12 +4,15 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { Box } from "@mui/system";
 import MobileFAQ from "./MobileFAQ";
 import Navbar from "./Navbar";
-import { Typography, Grid } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Typography, Grid, AppBar } from "@mui/material";
+import { NavHashLink } from "react-router-hash-link";
+import { useLocation } from "react-router-dom";
 
 const FAQs = () => {
   const theme = useTheme();
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  const { hash } = useLocation();
+  const isActive = (iHash) => hash === iHash;
   return (
     <div>
       <Navbar />
@@ -25,7 +28,7 @@ const FAQs = () => {
           <Box
             sx={{
               mt: 6,
-              marginRight: 10,
+              marginRight: 20,
               padding: 5,
               boxShadow: 3,
               borderRadius: 2,
@@ -34,22 +37,76 @@ const FAQs = () => {
               top: -1,
             }}
           >
-            <Box>
+            <Box sx={{ ml: 10, mt: 5 }}>
               <Typography variant="h4">FAQ Section</Typography>
-              <Box sx={{ bgcolor: "#59CE8F20", mt: 3 }}>
-                <NavLink to="#booking">
+              <Box sx={{ bgcolor: "#59CE8F20", mt: 3, pr: 5, pl: 1 }}>
+                <NavHashLink
+                  to="#Booking"
+                  style={
+                    isActive("#Booking")
+                      ? {
+                          color: "#59ce8f",
+                        }
+                      : {}
+                  }
+                >
                   <Typography sx={{ p: 1.5 }}>Booking</Typography>
-                </NavLink>
+                </NavHashLink>
+                <NavHashLink
+                  to="#BookingCancellation"
+                  style={
+                    isActive("#BookingCancellation")
+                      ? {
+                          color: "#59ce8f",
+                        }
+                      : {}
+                  }
+                >
+                  <Typography sx={{ p: 1.5 }}>Booking Cancellation</Typography>
+                </NavHashLink>
+                <NavHashLink
+                  to="#SafetyAndTips"
+                  style={
+                    isActive("#SafetyAndTips")
+                      ? {
+                          color: "#59ce8f",
+                        }
+                      : {}
+                  }
+                >
+                  <Typography sx={{ p: 1.5 }}>Safety and Tips</Typography>
+                </NavHashLink>
+                <NavHashLink
+                  to="#SecurityDeposit"
+                  style={
+                    isActive("#SecurityDeposit")
+                      ? {
+                          color: "#59ce8f",
+                        }
+                      : {}
+                  }
+                >
+                  <Typography sx={{ p: 1.5 }}>Security Deposit</Typography>
+                </NavHashLink>
 
-                <Typography sx={{ p: 1.5 }}>Booking Cancellation</Typography>
-                <Typography sx={{ p: 1.5 }}>Safety and Tips</Typography>
-                <Typography sx={{ p: 1.5 }}>Security Deposit</Typography>
-                <Typography sx={{ p: 1.5 }}>Accident ot Damage</Typography>
+                <NavHashLink
+                  to="#AccidentOrDamage"
+                  style={
+                    isActive("#AccidentOrDamage")
+                      ? {
+                          color: "#59ce8f",
+                        }
+                      : {}
+                  }
+                >
+                  <Typography sx={{ p: 1.5 }}>Accident or Damage</Typography>
+                </NavHashLink>
               </Box>
             </Box>
           </Box>
 
-          <Box sx={{ pl: "20%" }}>
+          <Box sx={{ pl: "30%", pr: "10%" }}>
+            <AppBar />
             <MobileFAQ />
           </Box>
         </Box>
