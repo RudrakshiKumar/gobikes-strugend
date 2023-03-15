d682627a50b848863e14e94ac17223b1a882f84c
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2023-03-15 19:46:48 +0530

    added NoBikeAvailable

--- src/components/NoBikeAvailable.js
+++ src/components/NoBikeAvailable.js
@@ -1,29 +1,27 @@
-import { Box, Container, IconButton, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import EventBusyIcon from "@mui/icons-material/EventBusy";
 import React from "react";
 
 export default function NoBikeAvailable() {
   return (
     <>
-      <Container sx={{ marginTop: "2%" }}>
-        <Box
-          sx={{
-            borderRadius: "5px",
-            boxShadow: "3",
-            paddingTop: "20%",
-            paddingBottom: "20%",
-            paddingLeft: "43%",
-          }}
-        >
-          <IconButton sx={{ color: "#59CE8F", marginLeft: "10%" }} size="large">
-            <EventBusyIcon fontSize="large" />
-          </IconButton>
-          <br />
-          <Typography variant="p" sx={{ fontSize: "23px" }}>
-            No Bike Available
-          </Typography>
-        </Box>
-      </Container>
+      <Box
+        sx={{
+          borderRadius: "5px",
+          boxShadow: "2",
+          paddingTop: "20%",
+          paddingBottom: "20%",
+          paddingLeft: "43%",
+        }}
+      >
+        <IconButton sx={{ color: "#59CE8F", marginLeft: "10%" }} size="large">
+          <EventBusyIcon fontSize="large" />
+        </IconButton>
+        <br />
+        <Typography variant="p" sx={{ fontSize: "23px" }}>
+          No Bike Available
+        </Typography>
+      </Box>
     </>
   );
 }
